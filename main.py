a1073439a0679e414e2436d40380349cfe3ef10d
twentiecker <twentiecker@gmail.com>
2022-05-19 13:14:54 +0700

    add myname

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 
 st.title('Uber pickups in NYC')
+st.write('by Lukman Eka Arifandhi')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
